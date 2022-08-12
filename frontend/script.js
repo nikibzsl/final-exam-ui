ed930d9f2c14f5dda27948db3b8852b367941752
nikibzsl <bzslniki@gmail.com>
2022-08-12 13:12:00 +0200

    changes, html instead js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-const headerComponent = function() {
+
+/* const headerComponent = function() {
    return `
     <header>
 	<h3 class="logo">${books.logo}</h3>
@@ -9,13 +10,33 @@ const headerComponent = function() {
     `
 }
 
+const cardComponent = function (sub, title, text) {
+    return `
+    <div class="books-card-container">
+        <h3 class="book-sub">${sub}</h3>
+        <h2 class="book-title">${title}</h2>
+        <p class="book-text">${text}</p>
+    </div>
+    <div class="button-container"
+        <button class="read-more">${books.button}</button>
+     <span class="material-symbols-outlined">
+        arrow_forward
+        </span>
+    </div>
+    `
+}
+
+
 
 const loadEvent = function () {
     const rootElement = document.getElementById("root")
 
+
+
     rootElement.insertAdjacentHTML("beforeend", headerComponent())
-    
+    rootElement.insertAdjacentHTML("beforeend", cardComponent())
+    rootElement.insertAdjacentHTML("beforeend", cardsContainer())
 
 }
 
-window.addEventListener("load", loadEvent)
+window.addEventListener("load", loadEvent) */
